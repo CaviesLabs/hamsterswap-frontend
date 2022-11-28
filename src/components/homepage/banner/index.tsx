1a26782a8d7c4ee56a8687e83c18d1f478afd345
lone wolf <leviethung2403@gmail.com>
2022-11-28 07:58:24 +0700

    fix heading banner

--- src/components/homepage/banner/index.tsx
+++ src/components/homepage/banner/index.tsx
@@ -7,24 +7,22 @@ export function Banner() {
 
   return (
     <div className={styles.bannerWrap}>
-      <div className="lg:max-w-[1180px] mx-auto h-full flex">
-        <div className="w-3/4 relative h-full">
-          <div className={styles.heading}>
-            <h1 className="text-6xl leading-tight">
-              Trustless P2P Swaps for Digital Collectibles and Assets
-            </h1>
-          </div>
-          <div className="absolute left-0 bottom-40">
-            <Button
-              className="!rounded-[100px] after:!rounded-[100px] !px-[20px] mt-4"
-              text="Create a Proposal"
-              size="large"
-              onClick={() => router.push("create-proposal")}
-            />
-          </div>
+      <div className="lg:max-w-[1180px] mx-auto h-full flex items-center">
+        <div className="w-2/3">
+          <h1 className="text-6xl leading-tight">
+            Trustless P2P Swaps for Digital Collectibles and Assets
+          </h1>
+          <Button
+            className="!rounded-[100px] after:!rounded-[100px] !px-[20px] mt-4"
+            text="Create a Proposal"
+            size="large"
+            onClick={() => router.push("create-proposal")}
+          />
         </div>
-        <div className="h-full flex items-center">
-          <img src="/assets/images/banner.png" />
+        <div className="w-1/3 h-full flex items-center relative">
+          <div className={styles.heading}>
+            <img src="/assets/images/banner.png" />
+          </div>
         </div>
       </div>
     </div>
