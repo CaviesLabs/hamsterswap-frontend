791e56afbabc82075909c510f551ec4fbca3d94d
lone wolf <leviethung2403@gmail.com>
2022-11-30 16:18:51 +0700

    feat: handle click on tab to navigate to site /u/<user_id>/<site_path>

--- src/components/user/sub-menu.tsx
+++ src/components/user/sub-menu.tsx
@@ -1,31 +1,37 @@
 import { FC } from "react";
+import { useRouter } from "next/router";
 import classnames from "classnames";
 import { SubMenuProps } from "@/src/components/user/types";
 
 const SubMenu: FC<SubMenuProps> = (props) => {
+  const router = useRouter();
+  const { id: userId } = router.query;
   const { curTab } = props;
   /**
    * TODO filter sub-menu items for public and personal view
    * only show Proposal and History for public sight
-   * TODO handle click on tab to navigate to site /u/<user_id>/<site_path>
    */
   return (
     <div className="py-[40px]">
       <div className="flex justify-center items-center">
         {[
-          ["Proposal"],
-          ["History"],
-          ["Game ID"],
-          ["Payment"],
-          ["Contacts"],
+          ["Proposal", "profile"],
+          ["History", "history"],
+          ["Game ID", "#"],
+          ["Payment", "payment"],
+          ["Contacts", "contacts"],
         ].map((item, index) => (
-          <div key={`tabt-title-${index}`} className="px-[40px] h-[30px]">
+          <div
+            onClick={() => router.push(`/u/${userId}/${item[1]}`)}
+            key={`tabt-title-${index}`}
+            className="px-[40px] h-[30px] cursor-pointer"
+          >
             <p
               className={classnames("px-[10px] cursor-pointer text-dark60", {
                 "!text-purple": index === curTab,
               })}
             >
-              {item}
+              {item[0]}
             </p>
             {index === curTab && (
               <div className="w-full h-[4px] bg-purple rounded-[3px] mt-[10px]"></div>
