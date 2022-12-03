4b36034298bf4b4e680ec930bdc83d424ef438d9
lone wolf <leviethung2403@gmail.com>
2022-12-03 11:43:41 +0700

    set default value for user contacts and update then

--- src/components/user/contact/index.tsx
+++ src/components/user/contact/index.tsx
@@ -3,51 +3,64 @@ import { EmailIcon, TelegramIcon, TwitterIcon } from "@/src/components/icons";
 import { Form } from "antd";
 import { userService } from "@/src/redux/saga/user/user.service";
 import { hProfileContactDto } from "@/src/dto/hProfile.dto";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 export const ContactForm = () => {
+  const [form] = Form.useForm();
+
+  const profile = useSelector((state: any) => state.hProfile);
+
   const onFinish = async (values: hProfileContactDto) => {
     await userService.updateUser(values);
   };
 
+  useEffect(() => {
+    profile && form.setFieldsValue(profile);
+  }, [profile]);
+
   return (
-    <Form name="basic" initialValues={{}} onFinish={onFinish} layout="vertical">
+    <Form
+      form={form}
+      name="basic"
+      initialValues={profile}
+      onFinish={onFinish}
+      layout="vertical"
+    >
       <Form.Item
         label={
-          <p className="flex">
+          <div className="flex">
             <TelegramIcon />
             <div className="ml-2">Telegram</div>
-          </p>
+          </div>
         }
         name="telegram"
       >
         <Input placeholder="Enter Telegram link" />
       </Form.Item>
-
       <Form.Item
         label={
-          <p className="flex">
+          <div className="flex">
             <TwitterIcon />
             <div className="ml-2">Twitter</div>
-          </p>
+          </div>
         }
         name="twitter"
       >
         <Input placeholder="Enter Twitter link" />
       </Form.Item>
-
       <Form.Item
         label={
-          <p className="flex">
+          <div className="flex">
             <EmailIcon />
             <div className="ml-2">Email</div>
-          </p>
+          </div>
         }
         name="email"
         rules={[{ type: "email", message: "Wrong email address" }]}
       >
         <Input placeholder="Enter email address" />
       </Form.Item>
-
       <Form.Item className="mt-8 flex justify-end">
         <Button text="Update" shape="primary" className="!px-10" />
       </Form.Item>
