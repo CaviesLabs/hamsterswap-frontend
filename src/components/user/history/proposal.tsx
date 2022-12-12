e467409b32be1b5081a7b9baae6571f0d21bf1a8
lone wolf <leviethung2403@gmail.com>
2022-12-12 14:16:55 +0700

    fix #120: switch data column if user is buyer

--- src/components/user/history/proposal.tsx
+++ src/components/user/history/proposal.tsx
@@ -6,15 +6,49 @@ import { DotIcon } from "@/src/components/icons";
 import { SwapProposalStatus } from "@/src/entities/proposal.entity";
 import { getStatus } from "@/src/utils/proposal-status";
 import dayjs from "dayjs";
+import { useWallet } from "@/src/hooks/useWallet";
 
 function Proposal(props: ProposalHistoryProps) {
   const router = useRouter();
   const { data } = props;
-  const status = data.status;
+  const { status, fulfillBy } = data;
+
+  /**
+   * @description
+   * validate user is buyer if wallet address match with fulfill user
+   */
+  const { solanaWallet } = useWallet();
+  const _isBuyer = fulfillBy === solanaWallet.publicKey?.toString();
 
   const swapOption = data.swapOptions.find(
     (_) => _.id === data.fulfilledWithOptionId
   );
+
+  const renderSwapItemCol = () =>
+    data.offerItems.map(({ id, nftMetadata }) => (
+      <div key={`swapItems-${id}`} className="flex items-center mb-3">
+        <img
+          className="w-10 rounded-lg"
+          src={nftMetadata.nft_image || nftMetadata.icon}
+        />
+        <p className="ml-2 text-lg">
+          {nftMetadata.nft_name || nftMetadata.symbol}
+        </p>
+      </div>
+    ));
+
+  const renderReceiveItemCol = () =>
+    swapOption?.items.map(({ id, nftMetadata }) => (
+      <div key={id} className="flex items-center mb-3">
+        <img
+          className="w-10 rounded-lg"
+          src={nftMetadata.nft_image || nftMetadata.icon}
+        />
+        <p className="ml-2 text-lg">
+          {nftMetadata.nft_name || nftMetadata.symbol}
+        </p>
+      </div>
+    ));
 
   return (
     <div className="border border-1 border-gray rounded-3xl p-6 mb-6">
@@ -23,17 +57,7 @@ function Proposal(props: ProposalHistoryProps) {
           {dayjs(data.createdAt).format(DATE_TIME_FORMAT)}
         </Col>
         <Col span={6}>
-          {data.offerItems.map(({ id, nftMetadata }) => (
-            <div key={`swapItems-${id}`} className="flex items-center mb-3">
-              <img
-                className="w-10 rounded-lg"
-                src={nftMetadata.nft_image || nftMetadata.icon}
-              />
-              <p className="ml-2 text-lg">
-                {nftMetadata.nft_name || nftMetadata.symbol}
-              </p>
-            </div>
-          ))}
+          {_isBuyer ? renderReceiveItemCol() : renderSwapItemCol()}
 
           <a
             className="text-md text-purple hover:underline cursor-pointer"
@@ -43,26 +67,13 @@ function Proposal(props: ProposalHistoryProps) {
           </a>
         </Col>
         <Col span={6}>
-          {swapOption?.items.map(({ id, nftMetadata }) => (
-            <div key={id} className="flex items-center mb-3">
-              <img
-                className="w-10 rounded-lg"
-                src={nftMetadata.nft_image || nftMetadata.icon}
-              />
-              <p className="ml-2 text-lg">
-                {nftMetadata.nft_name || nftMetadata.symbol}
-              </p>
-            </div>
-          ))}
+          {_isBuyer ? renderSwapItemCol() : renderReceiveItemCol()}
         </Col>
         <Col span={4} className="text-lg">
           <div>
             You ({utilsProvider.makeShort(data.ownerAddress ?? " ", 4)})
           </div>
           <img className="w-6 my-2" src="/assets/images/arrow-two-way.svg" />
-            className="w-[19px] my-2"
-            src="/assets/images/arrow-two-way.svg"
-          />
           <div
             className="text-[#735CF7] cursor-pointer"
             onClick={() => router.push(`/u/${data.fulfillByUserId}/profile`)}
