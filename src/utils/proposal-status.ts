598895e425e8706b6a515056ba3c7d8c6ab7d6dd
systemis <cuthinh2002@icloud.com>
2023-02-07 16:56:13 +0700

    fix: history

--- src/utils/proposal-status.ts
+++ src/utils/proposal-status.ts
@@ -3,7 +3,8 @@ import { SwapProposalStatus } from "@/src/entities/proposal.entity";
 export const getStatus = (status: SwapProposalStatus) => {
   switch (status) {
     case SwapProposalStatus.CANCELED:
-      return "Cancel";
+    case SwapProposalStatus.WITHDRAWN:
+      return "Canceled";
     case SwapProposalStatus.DEPOSITED:
       return "Deposited";
     case SwapProposalStatus.FULFILLED:
